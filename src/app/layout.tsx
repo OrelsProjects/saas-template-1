2fa34392bc1ef8540674aee8e1b49d39a19e80b6
orelsprojects <orelsmail@gmail.com>
2024-08-16 10:45:03 +0300

    Fix git errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./providers/StoreProvider";
 import AuthProvider from "./providers/AuthProvider";
-import { SessionProvider } from "next-auth/react";
 import SessionWrapper from "./providers/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
